f4014094489783a3a58d2194a28d2aa2eeb548a1
mustafaag <mustafaaga23@gmail.com>
2020-03-17 14:17:32 +0100

    Todo app finished

--- App.js
+++ App.js
@@ -1,36 +1,47 @@
 import React, {useState} from 'react';
-import { StyleSheet, Text, View, FlatList, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import Header from './components/Header';
+import TodoItem from './components/TodoItem';
+import AddTodo from './components/AddTodo';
 
 export default function App() {
-  const [people, setPeople] = useState([
-    {name : 'hello', id:1 },
-    {name : 'test', id:2 },
-    {name : 'test1', id:3 },
-    {name : 'test2', id:4 },
-    {name : 'test3', id:5 },
-    {name : 'test4', id:6 },
-    {name : 'test5', id:7 },
-    {name : 'test6', id:8 },
-  ]);
+  const [todos, setTodos] = useState([
+    {text: 'test1', key: '1' },
+    {text: 'test2', key: '2' },
+    {text: 'test3', key: '3' },
+    {text: 'test4', key: '4' },
+  ])
 
-  const pressHandler = (id) => {
-    setPeople((prevPeople)=>{
-      return prevPeople.filter(person => person.id != id);
+  const pressHandler = (key) =>{
+    setTodos((prevTodos) =>{
+      return prevTodos.filter(x=> x.key != key);
     });
   }
-
+  const submitHandler = (text) =>{
+    setTodos((prevTodos) =>{
+      return [
+        {text:text , key: Math.random().toString()},
+        ...prevTodos
+      ]
+    })
+  }
   return (
     <View style={styles.container}>
-     <FlatList 
-        numColumns={2}
-        keyExtractor={ (item)=>{ return item.id}}
-        data={people}
-        renderItem= {({ item })=> (
-          <TouchableOpacity onPress={()=> pressHandler(item.id)}>
-            <Text style={styles.item}> { item.name }</Text> 
-          </TouchableOpacity>          
-        )}
-     />
+      {/* Header */}
+      <Header />
+      <View style={styles.content}>
+        <AddTodo pressHandler={submitHandler}/>
+        <View style={styles.list}> 
+          <FlatList 
+            data={ todos }
+            renderItem={({ item })=>(
+              <TodoItem item={item} pressHandler={pressHandler} />
+              )             
+            }
+          />
+        </View>
+      </View>
+
     </View>
   );
 }
@@ -39,17 +50,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    paddingTop: 40,
-    paddingHorizontal:20,
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-  item: {
-    marginTop:24,
-    padding:30,
-    backgroundColor: 'pink',
-    fontSize:24,
-    marginHorizontal:10,
-    marginTop:24
+  content:{
+    padding:40,
+  },
+  list: {
+    marginTop:20
   }
 });
